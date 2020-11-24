12a523b73a194304de8a84ae210ffabbfbc002b4
Svetlin Pavlov <svetlinpav@svetlinpav@gmail.com>
2020-11-24 14:47:55 +0200

    One of final releases

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import HyLogo from "./hyflixlogo.png";
+import ProfileAvatar from "./profileavatar.jpg";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 80) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__solid"}`}>
+      <img src={HyLogo} alt="Hyflix Logo" className="nav__logo" />
+      <img src={ProfileAvatar} alt="Hyflix Avatar" className="nav__avatar" />
+    </div>
+  );
+}
+
+export default Nav;
